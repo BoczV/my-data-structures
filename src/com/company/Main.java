1c1895c4db90aa34d0aeb6df3cdb3e749b638deb
Viktor <beviktor95@gmail.com>
2021-04-17 15:31:19 +0200

    Create remove, add, insert, contains, size functionalities to MyArrayList class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,17 @@
 package com.company;
 
 
+import com.company.arraylist.MyArrayList;
 import com.company.linkedlist.MyLinkedList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+        /* MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
         myLinkedList.add(3);
         myLinkedList.add(1);
         myLinkedList.add(210);
@@ -18,6 +23,18 @@ public class Main {
         myLinkedList.insert(3, 1);
         System.out.println(myLinkedList);
         myLinkedList.sort();
-        System.out.println(myLinkedList);
+        System.out.println(myLinkedList);*/
+        MyArrayList<String> myArrayList = new MyArrayList<>();
+        myArrayList.add("Hi!");
+        myArrayList.add("Nice");
+        myArrayList.add("to");
+        myArrayList.add("see");
+        myArrayList.add("you");
+        System.out.println(myArrayList);
+        myArrayList.insert(5, "Bye");
+        System.out.println(myArrayList);
+        myArrayList.remove("Hi!");
+        System.out.println(myArrayList);
+        System.out.println(myArrayList.contains("Hi!"));
     }
 }
--- src/com/company/arraylist/MyArrayList.java
+++ src/com/company/arraylist/MyArrayList.java
@@ -1,4 +1,82 @@
 package com.company.arraylist;
 
-public class MyArrayList {
+import java.util.Arrays;
+import java.util.Collection;
+
+public class MyArrayList<T> {
+    private Object[] array;
+
+    public MyArrayList(T[] array) {
+        this.array = array;
+    }
+
+
+    public MyArrayList(Collection<T> collection){
+        array = collection.toArray();
+    }
+
+    public MyArrayList(int sizeOfCollection){
+        array = new Object[sizeOfCollection];
+    }
+
+    public MyArrayList() {
+        array = new Object[]{};
+    }
+
+    public void add(T newElement){
+        Object[] newArray = new Object[array.length + 1];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        newArray[array.length] = newElement;
+        array = newArray;
+    }
+
+    public void insert(int index, T newElement){
+        if(index > array.length) throw new IllegalArgumentException();
+        Object[] newArray = new Object[array.length + 1];
+        int innerIndex = 0;
+        for(int i = 0; i < newArray.length; i++){
+            if(i != index){
+                newArray[i] = array[i - innerIndex];
+            } else {
+                newArray[i] = newElement;
+                innerIndex++;
+            }
+        }
+        array = newArray;
+    }
+
+    public void remove(T removeElement){
+        if(contains(removeElement)){
+            Object[] newArray = new Object[array.length - 1];
+            int innerIndex = 0;
+            for(int i = 0; i < array.length; i++){
+                if(array[i] != removeElement){
+                    newArray[i - innerIndex] = array[i];
+                } else {
+                    innerIndex++;
+                }
+            }
+            array = newArray;
+        }
+    }
+
+    public boolean contains(T element){
+        boolean contains = false;
+        for (Object o : array) {
+            if (o == element) {
+                contains = true;
+                break;
+            }
+        }
+        return contains;
+    }
+
+    public int size(){
+        return array.length;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(array);
+    }
 }
