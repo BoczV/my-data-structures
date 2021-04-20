d8c572642c8fdcf20cfd03433e3f4dcf20540b49
Viktor <beviktor95@gmail.com>
2021-04-20 18:58:44 +0200

    Create test template to MyLinkedList and MyArrayList

--- src/test/java/com/company/arraylist/MyArrayListTest.java
+++ src/test/java/com/company/arraylist/MyArrayListTest.java
@@ -1,4 +1,69 @@
+package com.company.arraylist;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MyArrayListTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void testAddAll() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void toArray() {
+    }
+
+    @Test
+    void getFirst() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void sort() {
+    }
 }
