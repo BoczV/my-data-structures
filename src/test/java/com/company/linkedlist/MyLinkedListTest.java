9cdc488cebff14b8768f83967f906a90f3184fcc
Viktor <beviktor95@gmail.com>
2021-04-20 18:57:15 +0200

    Add maven to the project

---
+++ src/test/java/com/company/linkedlist/MyLinkedListTest.java
@@ -0,0 +1,66 @@
+package com.company.linkedlist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyLinkedListTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testRemove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFirst() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toArray() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sort() {
+    }
+}
