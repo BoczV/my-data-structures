340a4ccd34e5d90804245ba51c00a3563806fda1
Viktor <beviktor95@gmail.com>
2021-04-20 19:14:54 +0200

    MyLinkedList tests added: setup() test, add() test, addAllArrayElement() test

--- src/test/java/com/company/linkedlist/MyLinkedListTest.java
+++ src/test/java/com/company/linkedlist/MyLinkedListTest.java
@@ -1,31 +1,50 @@
 package com.company.linkedlist;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 
 class MyLinkedListTest {
 
+    private MyLinkedList<Integer> myIntegerLinkedList;
+    private MyLinkedList<String> myStringLinkedList;
+    private String[] stringArray;
+    private Integer[] intArray;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        myIntegerLinkedList = new MyLinkedList<>();
+        myStringLinkedList = new MyLinkedList<>();
+        stringArray = new String[]{"Pear", "Cherry"};
+        intArray = new Integer[]{3, 1, 8, 2};
     }
 
     @org.junit.jupiter.api.Test
     void add() {
+        myIntegerLinkedList.add(5);
+        myIntegerLinkedList.add(1);
+        myStringLinkedList.add("Alma");
+        Assertions.assertEquals(2, myIntegerLinkedList.size());
+        Assertions.assertEquals(1, myStringLinkedList.size());
     }
 
     @org.junit.jupiter.api.Test
-    void addAll() {
+    void addAllArrayElement() {
+        myIntegerLinkedList.addAll(intArray);
+        myStringLinkedList.addAll(stringArray);
+        Assertions.assertEquals("[3, 1, 8, 2]", myIntegerLinkedList.toString());
+        Assertions.assertEquals("[Pear, Cherry]", myStringLinkedList.toString());
     }
 
     @org.junit.jupiter.api.Test
-    void testAddAll() {
+    void addAllCollectionElement() {
     }
 
     @org.junit.jupiter.api.Test
-    void remove() {
+    void removeById() {
     }
 
     @org.junit.jupiter.api.Test
-    void testRemove() {
+    void removeByElement() {
     }
 
     @org.junit.jupiter.api.Test
