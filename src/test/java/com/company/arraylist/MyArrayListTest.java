9f46934968c67944a6aacf4d1ef8997c95c572b9
Viktor <beviktor95@gmail.com>
2021-04-23 13:31:24 +0200

    Removed unneccesary parts of test class

--- src/test/java/com/company/arraylist/MyArrayListTest.java
+++ src/test/java/com/company/arraylist/MyArrayListTest.java
@@ -1,6 +1,5 @@
 package com.company.arraylist;
 
-import com.company.linkedlist.MyLinkedList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
