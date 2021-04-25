61367d6c205b6e93b9ef266a931099e0bd5b76e9
Viktor <beviktor95@gmail.com>
2021-04-25 11:55:32 +0200

    Changed test case

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,24 +1,9 @@
 package com.company;
 
 
-import com.company.arraylist.MyArrayList;
-import com.company.linkedlist.MyLinkedList;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        MyLinkedList<Integer> myIntegerLinkedList = new MyLinkedList<>();
-        myIntegerLinkedList.add(45);
-        myIntegerLinkedList.add(61);
-        myIntegerLinkedList.add(3);
-        myIntegerLinkedList.add(1);
-        myIntegerLinkedList.add(8);
-        myIntegerLinkedList.add(2);
-        myIntegerLinkedList.sort();
-        System.out.println(myIntegerLinkedList);
     }
 }
